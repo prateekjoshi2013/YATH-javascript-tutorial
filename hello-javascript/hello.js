6f337dd3ba1e079fc48306b4cb4d4e73eaadc3fc
Prateek Joshi <prateekjoshi2013@gmail.com>
2023-05-05 14:28:45 -0700

    added part-1-solution

---
+++ hello-javascript/hello.js
@@ -0,0 +1,29 @@
+
+// This is how we print to the console in javascript
+console.log("I am your Instructor")
+
+// This is a single line comment in Javascript
+
+/*
+    This is how we write 
+    multiline comments in javascript
+*/
+
+const helloWorld =() => {
+    //======WRITE YOUR CODE (PROMPT-1)======//
+    
+    
+    // this is my single line comment
+
+    /*
+        This is my
+        multiline comment 
+    */
+    console.log("Hello Javascript");
+    
+
+    //======YOUR CODE ENDS (PROMPT-1)======//
+
+}
+
+module.exports=helloWorld;
