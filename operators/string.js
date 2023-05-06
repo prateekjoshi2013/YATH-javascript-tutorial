d721f00bf57bc440a887419bdd394fdfa4716756
Prateek Joshi <prateekjoshi2013@gmail.com>
2023-05-05 20:56:57 -0700

    added part-4 solutions

---
+++ operators/string.js
@@ -0,0 +1,15 @@
+/*
+ concatenation on string can be done using + operation 
+  
+ "a" + "b"
+=> "ab"
+
+*/
+
+const checkStringConcatenation = () => {
+    console.log(
+        ("a" + "b") === "ab"
+    )
+}
+
+module.exports = checkStringConcatenation
